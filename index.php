a22dd1b3860d92979f251e12fdb7196782f9c73e
Riley Weber <rileypweber@gmail.com>
2017-02-06 14:08:20 -0500

    asdf

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
 <head>
 	<title>Riley P Weber</title>
+	<link rel="stylesheet" type="text/css" href="/bin/styles/fonts/fonts.css">
 	<link rel="stylesheet" type="text/css" href="/bin/styles/home.css">
 </head>
 <body>
-
+<div class="me">Riley Weber</div>
+<div class="nav">
+	<div class="nav-item">HOME</div><div class="nav-item">ABOUT</div><div class="nav-item">PROJECTS</div>
+</div>
 </body>
 </html>
